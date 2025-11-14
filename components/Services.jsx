a1b125acb3b438644f2228d4dbdbd486fdad2f3e
Mario <mario_bianchi_jr@hotmail.com>
2025-11-14 13:58:09 +0100

    list and map for my grid

--- components/Services.jsx
+++ components/Services.jsx
@@ -1,5 +1,37 @@
 import React from 'react'
 
+const services = [
+  {
+    title: "Branding",
+    text: "Egestas tellus nunc proin amet tellus tincidunt lacus consequat. Ultrices"
+  },
+  {
+    title: "Web development",
+    text: "Integer ante non nunc, eget est justo vel semper nunc. Lacus."
+  },
+  {
+    title: "Digital marketing",
+    text: "Sed faucibus faucibus egestas volutpat, accumsan adipiscing egestas est. Auctor et leo urna est."
+  },
+  {
+    title: "Programmatic advertising",
+    text: "Sed faucibus faucibus egestas volutpat, accumsan adipiscing egestas est. Auctor et leo urna est."
+  },
+  {
+    title: "Mobile App",
+    text: "Egestas tellus nunc proin amet tellus tincidunt lacus consequat. Ultrices."
+  },
+  {
+    title: "SEO",
+    text: "Integer ante non nunc, eget est justo vel semper nunc. Lacus."
+  },
+  { 
+  title: "User testing",
+    text: "Sed faucibus faucibus egestas volutpat, accumsan adipiscing egestas est. Auctor et leo urna est."
+  }
+];
+
+
 const Services = () => {
   return (
     <section className="bg-[#0b0b0b] text-white py-24">
@@ -29,17 +61,19 @@ const Services = () => {
       </p>
 
       <div className="mt-12 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
-        {[...Array(6)].map((_, i) => (
+        {services.map((service, i) => (
           <div
-            key={i}
-            className="bg-[#141414] border border-[#1f1f1f] hover:border-[#2b2b2b] transition rounded-2xl p-8 text-left"
-          >
-            <div className="w-10 h-10 bg-orange-500/20 rounded-lg mb-4"></div>
-            <h3 className="text-xl font-semibold mb-2">Branding</h3>
-            <p className="text-gray-500 text-[11px] tracking-[0.15em] leading-tight max-w-sm">
-            Egestas tellus nunc proin amet tellus tincidunt lacus consequat. Ultrices
-            </p>
-          </div>
+          key={i}
+          className="bg-[#141414] border border-[#1f1f1f] hover:border-[#2b2b2b] transition rounded-2xl p-8 text-left"
+        >
+          <div className="w-10 h-10 bg-orange-500/20 rounded-lg mb-4"></div>
+        
+          <h3 className="text-xl font-semibold mb-2">{service.title}</h3>
+        
+          <p className="text-gray-500 text-[11px] tracking-[0.15em] leading-tight max-w-sm">
+            {service.text}
+          </p>
+        </div>
         ))}
       </div>
     </div>
